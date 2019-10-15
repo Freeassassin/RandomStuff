60d957dea44172247cb4d0afc9b6775f9edd416e
Freeassassin <farbodemzadeh@gmail.com>
2019-10-15 00:21:38 -0400

    Update ChemGame.py

--- ChemGame.py
+++ ChemGame.py
@@ -2,7 +2,9 @@ import pygame
 pygame.init()
 
 class button():
+
     def __init__(self, color, x,y,width,height, text=''):
+
         self.color = color
         self.x = x
         self.y = y
@@ -18,6 +20,7 @@ class button():
         pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
         
         if self.text != '':
+
             FONT = pygame.font.SysFont('cour.ttf', 40)
             text = FONT.render(self.text, 1, (0,0,0))
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
