915166392b1f152de6f6b423baf6a5567e8eb28d
Freeassassin <farbodemzadeh@gmail.com>
2020-03-12 08:22:43 -0400

    2020-03-12

--- Physics/SimpleMotion.py
+++ Physics/SimpleMotion.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
 import math
 import os
 clear = lambda : os.system('cls')
@@ -7,14 +9,10 @@ try:
 except Exception as e:
 
 	deltaT = None
-
-
 try:
 	deltaD = float(input("please enter Δd: "))
 except Exception as e:
 	deltaD = None
-
-
 try:
 	initialV = float(input("please enter Initial Velocity: "))
 	
@@ -27,86 +25,130 @@ except Exception as e:
 	finalV = None
 try:
 	
-	a = float(input("please enter Acceleration: "))
+	accel = float(input("please enter Acceleration: "))
 except Exception as e:
-	a = None
+	accel = None
 
 
 clear()
 
 
 if deltaD == None:
-	if a == None:
+	if accel == None:
 		deltaD = ((initialV + finalV)/2)*deltaT
+		print("Calculated Δd Using deltaT((initialV + finalV)/2)")
 	elif finalV == None:
-		deltaD = initialV*deltaT+(a*(deltaT*deltaT))/2
+		deltaD = initialV*deltaT+(accel*(deltaT*deltaT))/2
+		print("Calculated Δd Using initialV*deltaT+(a*(deltaT^2))/2")
 	elif initialV == None:
-		deltaD = finalV*deltaT-(a*(deltaT*deltaT))/2
+		deltaD = finalV*deltaT-(accel*(deltaT*deltaT))/2
+		print("Calculated Δd Using finalV*deltaT-(a*(deltaT^2))/2")
 	elif deltaT == None:
-		deltaD = ((finalV*finalV)-(initialV*initialV))/2*a
+		deltaD = ((finalV*finalV)-(initialV*initialV))/2*accel
+		print("Calculated Δd Using ((finalV^2)-(initialV^2))/2*a")
 	else:
 		deltaD = ((initialV + finalV)/2)*deltaT
+		print("Calculated Δd Using ((initialV + finalV)/2)*deltaT")
 
 
 
 if finalV == None:
-	if a == None:
+	if accel == None:
 		finalV = 2*(deltaD/deltaT)-initialV
+		print("Calculated Final Velocity Using 2*(deltaD/deltaT)-initialV")
 	elif initialV == None:
-		finalV = (deltaD + (a*(deltaT*deltaT))/2)/deltaT
+		finalV = (deltaD + (accel*(deltaT*deltaT))/2)/deltaT
+		print("Calculated Final Velocity Using (deltaD + (a*(deltaT^2))/2)/deltaT")
 	elif deltaT == None:
-		finalV = math.sqrt((initialV*initialV)+2*a*deltaD)
+		finalV = math.sqrt((initialV*initialV)+2*accel*deltaD)
+		print("Calculated Final Velocity Using ((initialV*initialV)+2*a*deltaD)^0.5")
 	elif deltaD == None:
 		finalV = initialV + a*deltaT
+		print("Calculated Final Velocity Using initialV + a*deltaT")
 	else:
-		finalV = initialV + a*deltaT
+		finalV = initialV + accel*deltaT
+		print("Calculated Final Velocity Using initialV + a*deltaT")
 
 
 
 if initialV == None:
-	if a == None:
+	if accel == None:
 		initialV = 2*(deltaD/deltaT) - finalV
+		print("Calculated Initial Velocity Using 2*(deltaD/deltaT) - finalV")
 	elif finalV == None:
-		initialV = (deltaD - (a*(deltaT*deltaT))/2)/deltaT
+		initialV = (deltaD - (accel*(deltaT*deltaT))/2)/deltaT
+		print("Calculated Initial Velocity Using (deltaD - (a*(deltaT^2))/2)/deltaT")
 	elif deltaD == None:
-		initialV = finalV - a*deltaT
+		initialV = finalV - accel*deltaT
+		print("Calculated Initial Velocity Using finalV - a*deltaT")
 	elif deltaT == None:
-		initialV = math.sqrt((finalV*finalV)-2*a*deltaD)
+		initialV = math.sqrt((finalV*finalV)-2*accel*deltaD)
+		print("Calculated Initial Velocity Using ((finalV*finalV)-2*a*deltaD)^0.5")
 	else:
-		initialV = finalV - a*deltaT
+		initialV = finalV - accel*deltaT
+		print("Calculated Initial Velocity Using finalV - a*deltaT")
 
 
 
 if deltaT == None:
-	if a == None:
+	if accel == None:
 		deltaT = deltaD/((finalV+initialV)/2) 
+		print("Calculated Δt Using deltaD/((finalV+initialV)/2)")
 	elif deltaD == None:
-		deltaT = (finalV - initialV)/a
+		deltaT = (finalV - initialV)/accel
+		print("Calculated Δt Using (finalV - initialV)/a")
 	elif initialV == None:
-		deltaT = (-(finalV)+math.sqrt((finalV*finalV)-4*((-a)/2)*(-deltaD)) )/(-a)
+		deltaT = (-(finalV)+math.sqrt((finalV*finalV)-4*((-a)/2)*(-deltaD)))/(-accel)
+		print("Calculated Δt Using (-(finalV)+((finalV^2)-4*((-a)/2)*(-deltaD))^0.5)/(-a)")
 	elif finalV == None:
-		deltaT = ((-initialV)+math.sqrt((initialV*initialV)-4*(a/2)*(-deltaD)) )/a
+		deltaT = ((-initialV)+math.sqrt((initialV*initialV)-4*(a/2)*(-deltaD)) )/accel
+		print("Calculated Δt Using ((-initialV)+((initialV^2)-4*(a/2)*(-deltaD))^0.5)/a")
 	else:
-		deltaT = (finalV - initialV)/a
+		deltaT = (finalV - initialV)/accel
+		print("Calculated Δt Using (finalV - initialV)/a")
 
 
 	
-if a == None:
+if accel == None:
 	if deltaT == None:
-		a = ((finalV*finalV)-(initialV*initialV))/(2*deltaD)
+		accel = ((finalV*finalV)-(initialV*initialV))/(2*deltaD)
+		print("Calculated Acceleration Using ((finalV^2)-(initialV^2))/(2*deltaD)")
 	elif deltaD == None:
-		a = (finalV-initialV)/deltaT
+		accel = (finalV-initialV)/deltaT
+		print("Calculated Acceleration Using (finalV-initialV)/deltaT")
 	elif initialV == None:
-		a = -((2*(deltaD-(finalV*deltaT)))/(deltaT*deltaT))
+		accel = -((2*(deltaD-(finalV*deltaT)))/(deltaT*deltaT))
+		print("Calculated Acceleration Using -((2*(deltaD-(finalV*deltaT)))/(deltaT^2))")
 	elif finalV == None:
-		a = (2*(deltaD-(initialV*deltaT)))/(deltaT*deltaT)
+		accel = (2*(deltaD-(initialV*deltaT)))/(deltaT*deltaT)
+		print("Calculated Acceleration Using (2*(deltaD-(initialV*deltaT)))/(deltaT^2)")
 	else:
-		a = (finalV-initialV)/deltaT
+		accel = (finalV-initialV)/deltaT
+		print("Calculated Acceleration Using (finalV-initialV)/deltaT")
 
 print("Δd is equal to: ",deltaD)
 print("\nΔt is equal to: ",deltaT)
 print("\nInitial Velocity is equal to: ",initialV)
 print("\nFinal Velocity is equal to: ",finalV)
-print("\nAcceleration is equal to: ",a)
+print("\nAcceleration is equal to: ",accel)
 
 
+
+
+
+def d(t):
+    return ((initialV + finalV)/2)*t
+def a(t):
+	return (finalV-initialV)/t
+def v(t):
+	return initialV + accel*t
+
+t = np.arange(0.0, deltaT, 0.1)
+plt.figure()
+plt.subplot(211)
+plt.plot(t, d(t), 'r')
+plt.plot(t, a(t), 'k')
+plt.plot(t, v(t), 'g')
+plt.xlabel('Time (Seconds)')
+plt.ylabel('Δd(red), v(green), a(black)') 
+plt.show()
